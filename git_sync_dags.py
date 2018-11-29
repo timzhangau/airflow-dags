bcff25361241e15682d47574d4663b8b32dfba94
timzhangau <tim.zhang.au@gmail.com>
2018-11-30 00:06:32 +1100

    add git-sync and notification test dags

---
+++ git_sync_dags.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from utils.callback import slack_failure_callback, slack_success_callback
+
+
+
+default_args = {
+    'owner': 'airflow@spitfire',
+    'start_date': datetime(2018, 11, 23),
+    'retries': 1,
+    'email': ['tim@spitfire.io'],
+    'email_on_failure': False,
+    'depends_on_past': False,
+}
+
+
+# run every minute
+schedule = '*/15 * * * *'
+
+dag = DAG(
+    dag_id='git_sync_dags',
+    schedule_interval=schedule,
+    default_args=default_args,
+    catchup=False,
+)
+
+git_sync_bash = """
+    cd /usr/local/airflow/dags && git fetch origin && git reset --hard origin/master && git clean -f -d
+"""
+
+t1 = BashOperator(
+    task_id='git_fetch_and_reset',
+    bash_command=git_sync_bash,
+    on_success_callback=slack_success_callback,
+    on_failure_callback=slack_failure_callback,
+    dag=dag
+)
---
+++ test_failure_notification.py
@@ -0,0 +1,34 @@
+import datetime
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from utils.callback import slack_failure_callback, slack_success_callback
+
+
+
+default_args = {
+    'owner': 'airflow@spitfire',
+    'start_date': datetime.datetime(2018, 11, 27),
+    'retries': 0,
+    'email': ['tim@spitfire.io'],
+    'email_on_failure': False,
+    'depends_on_past': False,
+    'retry_delay': datetime.timedelta(seconds=30),
+}
+
+
+# run every minute
+schedule = '*/5 * * * *'
+
+dag = DAG(
+    dag_id='test_failure_notification',
+    schedule_interval=schedule,
+    default_args=default_args,
+    catchup=False,
+)
+
+t1 = BashOperator(
+    task_id='test_failure_notification_task',
+    bash_command="echo.py",
+    on_failure_callback=slack_failure_callback,
+    dag=dag
+)
