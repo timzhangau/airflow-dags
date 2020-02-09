723efa92147f3188fb7439964beecfdb56a1e6ca
timzhang <tim.zhang.au@gmail.com>
2020-02-09 17:11:36 +1100

    update afr spider task definition

--- terraform/news-feed-afr-spider.tf
+++ terraform/news-feed-afr-spider.tf
@@ -1,27 +1,27 @@
-data "template_file" "news-feed-s3-to-postgres-definition" {
+data "template_file" "news-feed-afr-spider-definition" {
   template = file("task-definition.json")
   vars = {
-    container_name   = "news-feed-s3-to-postgres"
+    container_name   = "news-feed-afr-spider"
     container_cpu    = 1024
     container_memory = 2048
-    image_url        = "timzhangau/pipeline"
+    image_url        = "timzhangau/scrapy"
     env              = "airflow"
-    log_group_name   = aws_cloudwatch_log_group.airflow-news-feed-s3-to-postgres.name
+    log_group_name   = aws_cloudwatch_log_group.airflow-news-feed-afr-spider.name
     log_group_region = "ap-southeast-2"
   }
 }
 
 
-resource "aws_cloudwatch_log_group" "airflow-news-feed-s3-to-postgres" {
-  name              = "airflow/news-feed-s3-to-postgres"
+resource "aws_cloudwatch_log_group" "airflow-news-feed-afr-spider" {
+  name              = "airflow/news-feed-afr-spider"
   retention_in_days = 30  # use 30 days as retention, the log will be retrieved and saved by airflow to s3
 }
 
-resource "aws_ecs_task_definition" "news-feed-s3-to-postgres" {
-  family                   = "news-feed-s3-to-postgres"
+resource "aws_ecs_task_definition" "news-feed-afr-spider" {
+  family                   = "news-feed-afr-spider"
   requires_compatibilities = ["FARGATE"]
   //  within container definitions, repositoryCredentials specifies the secrect manager to access private registry
-  container_definitions = data.template_file.news-feed-s3-to-postgres-definition.rendered
+  container_definitions = data.template_file.news-feed-afr-spider-definition.rendered
   task_role_arn         = aws_iam_role.airflow-ecs-role.arn
   execution_role_arn    = aws_iam_role.airflow-ecs-role.arn
   network_mode          = "awsvpc"
